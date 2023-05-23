4587d7fb5c1d027ed54383f70689994fb2cb8852
Alex Rodrigues <alex@newsolution.com.br>
2023-05-23 17:32:33 -0300

    Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // Carrosel 
 
 var swiper = new Swiper(".mySwiper", {
@@ -34,3 +33,52 @@ var swiper = new Swiper(".slide-ceo", {
     disableOnInteraction: false
   }
 });
+
+
+//Menu
+
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation ?
+        (link.style.animation = "") :
+        (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+ß
