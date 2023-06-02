8e1c9b357255aff427a6839ea07d1520dd5401bc
Alex Rodrigues <alex@newsolution.com.br>
2023-06-02 17:21:56 -0300

    Menu responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,47 +37,59 @@ var swiper = new Swiper(".slide-ceo", {
 
 //Menu
 
-class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
+/**
+ * Mobile nav toggle
+ */
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+const mobileNavShow = document.querySelector('.mobile-nav-show');
+const mobileNavHide = document.querySelector('.mobile-nav-hide');
 
-  animateLinks() {
-    this.navLinks.forEach((link, index) => {
-      link.style.animation ?
-        (link.style.animation = "") :
-        (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
-    });
-  }
+document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+  el.addEventListener('click', function (event) {
+    event.preventDefault();
+    mobileNavToogle();
+  })
+});
 
-  handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-  }
-
-  addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
-  }
-
-  init() {
-    if (this.mobileMenu) {
-      this.addClickEvent();
-    }
-    return this;
-  }
+function mobileNavToogle() {
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  mobileNavShow.classList.toggle('d-none');
+  mobileNavHide.classList.toggle('d-none');
 }
 
-const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li",
-);
-mobileNavbar.init();
+/**
+ * Hide mobile nav on same-page/hash links
+ */
+document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+  if (!navbarlink.hash) return;
+
+  let section = document.querySelector(navbarlink.hash);
+  if (!section) return;
+
+  navbarlink.addEventListener('click', () => {
+    if (document.querySelector('.mobile-nav-active')) {
+      mobileNavToogle();
+    }
+  });
+
+});
+
+/**
+ * Toggle mobile nav dropdowns
+ */
+const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+navDropdowns.forEach(el => {
+  el.addEventListener('click', function (event) {
+    if (document.querySelector('.mobile-nav-active')) {
+      event.preventDefault();
+      this.classList.toggle('active');
+      this.nextElementSibling.classList.toggle('dropdown-active');
+
+      let dropDownIndicator = this.querySelector('.dropdown-indicator');
+      dropDownIndicator.classList.toggle('bi-chevron-up');
+      dropDownIndicator.classList.toggle('bi-chevron-down');
+    }
+  })
+});
