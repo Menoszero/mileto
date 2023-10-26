6aa89dd46df1cc8aabd88965c224bb27c3611647
Alex Rodrigues <alex@newsolution.com>
2023-10-26 11:46:13 -0300

    Responsivo v3

--- assets/js/animacao.js
+++ assets/js/animacao.js
@@ -1,6 +1,4 @@
 
-// Configure o caminho para o arquivo de animação JSON do LottieFiles
-const animationPath = 'https://lottie.host/de429f84-e62f-40af-a109-de5ab6af24f7/P5wcANaTnx.json';
 
 // Obtenha uma referência para o botão WhatsApp
 const whatsappButton = document.getElementById('whatsapp-button');
